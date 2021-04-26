dd915cda288c7e130d838e77b413e364e5873d17
Vidyayani <vidyayani@gmail.com>
2021-04-26 17:58:53 +0530

    updated weather api with working code

---
+++ routes/weather.js
@@ -0,0 +1,46 @@
+const request = require('request')
+const express = require('express')
+const { response } = require('express')
+const router = express.Router()
+
+const options = {
+  method: 'GET',
+  url: 'https://community-open-weather-map.p.rapidapi.com/forecast',
+  qs: { q: 'bangalore,IND' },
+  headers: {
+    'x-rapidapi-key': '2efe6ff787msh76884ac9c5de4a0p1e000cjsn419a382c680c',
+    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+    useQueryString: true
+  }
+}
+
+
+router.get('', (req,res) => {
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error)
+        if(response.statusCode == '200'){
+
+            body = JSON.parse(body)
+        var array = body.list.filter(element => {return element.dt_txt.includes("09:00:00")});
+        var data_arr = []
+        var jsonresponse = { count : array.length,  unit : "metric", location: body.city.name}
+        array.forEach((element) => {
+
+            var json = {};
+            json.date = (new Date(element.dt_txt)).toDateString()
+            json.main = element.weather[0].description
+            json.temp = element.main.temp
+            data_arr.push(json)
+        });
+        jsonresponse.data = data_arr;
+        res.status(200).json(jsonresponse)
+        }else
+        res.status(response.statusCode).json(response.statusMessage)
+        console.log("Fetched response from open weather API \n ------------------------------- ", body)
+      })
+
+})
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const weatherRoute = require('./routes/weather')
 const newsRoute = require('./routes/news')
 
 app.use(express.json())
-app.use('/news', newsRoute)
+// app.use('/news', newsRoute)
 app.use('/weather', weatherRoute)
 
 app.listen(2400, () => { console.log('Server started: 2400') })
